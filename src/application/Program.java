20fcaa9c110dec474c567cf59de7c1d5eab89bf1
Emilly Fernandes <emilly.fernandesads@gmail.com>
2023-10-27 10:22:49 -0300

    Handling exceptions and clearing console

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -14,16 +16,23 @@ public class Program {
 
         while (true){
 
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
+            try{
+                UI.clearScreen();
 
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(scan);
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
 
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(scan);
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(scan);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(scan);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException | InputMismatchException e){
+                System.out.println(e.getMessage());
+                scan.nextLine();
+            }
         }
     }
 }
